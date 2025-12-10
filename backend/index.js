6862ace7f7c0f38f9f27c9fafb19690db995b445
subham samrat jena <subhamsamrat912424@gmail.com>
2025-12-11 00:41:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Your frontend origin
+  origin: 'https://empower-jx4r.onrender.com', // Your frontend origin
   credentials: true, // Allow credentials (cookies)
    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
