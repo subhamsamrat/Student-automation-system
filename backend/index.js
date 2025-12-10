ae6158f48089f703ba0c64339d5d316a302c8e24
subham samrat jena <subhamsamrat912424@gmail.com>
2025-12-11 01:31:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(fileUpload({
 //configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(cors());
+app.use(cors());
 app.use(cookieParser());
 
 const corsOptions = {
